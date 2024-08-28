c739f9808b02b4befe50f2d28a5a5eff6d8172ab
Bunleap Beun <bunleap997@gmail.com>
2024-08-28 10:50:54 +0700

    first commit

---
+++ Day 02 - Variable/script.js
@@ -0,0 +1,14 @@
+// Classwork - 01
+
+var temperature = 55;
+var condition = 'Sunny';
+var sunriseTime = "7:34 AM";
+var sunsetTime = "5:09 PM";
+
+console.log(temperature);
+console.log(condition);
+console.log(sunriseTime);
+console.log(sunsetTime);
+
+// console.log("it's currently " + condition + " and " + temperature + " degrees." +"The sun rise this morning at" + sunriseTime + "and will set at" + sunriseTime);
+console.log(`it's currently ${condition} and ${temperature} degrees. The sun rise this morning at ${sunriseTime} and will set att ${sunsetTime}`);
