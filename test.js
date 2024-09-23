a87f37a7cac98f6d7228e55e5559b6e3d0843151
Bunleap Beun <bunleap997@gmail.com>
2024-09-23 14:17:21 +0700

    DOM

--- test.js
+++ test.js
@@ -1,2 +1,4 @@
-console.log("Output : " + Math.acosh(0)); 
-console.log("Output : " + Math.acos(0)); 
+let numbers = [2, 5, 8, 3, 1];
+
+let minValue = Math.min(...numbers);
+console.log(minValue); // Output: 1
